d655e0af515eeee274eee30730402c0c5e68fda8
Gerben Speelpenning <gerben@speelpenning.nl>
2016-02-18 14:22:48 +0000

    Switched to file based sqlite db.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -44,7 +44,7 @@ abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase
     {
         config([
             'database.default' => 'sqlite',
-            'database.connections.sqlite.database' => ':memory:',
+//            'database.connections.sqlite.database' => ':memory:',
         ]);
     }
 }
